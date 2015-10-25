00165b49fe9b3097cbdf6e2135f05e2a15f3e151
RyanWilkie <ryanwilkie2@gmail.com>
2015-10-25 22:54:40 +0000

    remove some css from global

--- apply.php
+++ apply.php
@@ -1,13 +1,15 @@
 <<!DOCTYPE html>
 <html>
-<head>
-	<title>Twitch Familia - Apply</title>
-</head>
-<body>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css">
+		<title>Twitch Familia - Apply</title>
+	</head>
+	<body>
 
-	<?php include "navbar.html" ?>
+		<?php include "template/navbar.html" ?>
 
-	<iframe src="https://docs.google.com/forms/d/1dc28haCnzPBPr3dDvN3N5YOBZjn6IEvZVaPM1HNJSbI/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+		<iframe src="https://docs.google.com/forms/d/1dc28haCnzPBPr3dDvN3N5YOBZjn6IEvZVaPM1HNJSbI/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 
-</body>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -5,6 +5,74 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css">
 		<title>Twitch Familia</title>
+		<style>
+
+			/* STREAMS */
+
+			#streamWrapper {
+				margin-top: 15px;
+				margin-bottom: 10px;
+			}
+
+			/* centered columns styles */
+			.row-centered {
+			    text-align:center;
+			}
+
+			.col-centered {
+			    display:inline-block;
+			    float:none;
+			    /* reset the text-align */
+			    text-align:left;
+			    /* inline-block space fix */
+			    margin-right:-4px;
+			}
+
+			#streamsLoading {
+				height: 75px;
+				margin-top: 30px;
+			}
+
+			.streamer {
+				margin-top: 15px;
+				max-width: 100%;
+			}
+
+			.stream-profile {
+				position: absolute;
+				left: 0;
+				top: 5px;
+				height: 80px;
+				box-shadow: 0 3px 2px #142328;
+			}
+
+			.thumbDesc {
+				margin-top: 5px;
+				padding-left: 5px;
+				margin-left: 70px;
+				color: #303030;
+			}
+
+			.thumbDesc p {
+				text-decoration: none;
+			}
+
+			.title, .nameGame {
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				margin: 0;
+				padding: 0;
+				
+			}
+
+			.stream-preview {
+				margin-left: 25px;
+			}
+
+			/* END STREAMS */
+		
+		</style>
 	</head>
 	<body>
 
