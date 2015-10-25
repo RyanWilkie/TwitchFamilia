24af68f4d019138a4b5be83ba53d28e11250b480
RyanWilkie <ryanwilkie2@gmail.com>
2015-10-25 22:15:38 +0000

    create header.php

---
+++ template/header.php
@@ -0,0 +1,53 @@
+<div id="header">
+	<div class="row header-info">
+
+		<!-- display la familia logo -->
+		<div class="col-md-6">
+			<img class="img-responsive center-block logoIMG" src="img/whitelogo.png" alt='La Familia Logo'>
+		</div>
+
+		<!-- display info text -->
+		<div class="col-md-6 text">
+			<div class="middle">
+				<div class="inner">
+					<h1>Lorem ipsum dolor sit</h1>
+					<p>amet, consectetur adipiscing elit. Morbi varius tellus a ligula gravida ornare. Morbi lobortis nisl purus, vitae tempus ligula tincidunt eu. Quisque aliquet dui at ipsum cursus, a ultricies augue fringilla. Aenean commodo sed velit a ullamcorper. Duis mattis magna sapien, vel varius justo pellentesque et. Nullam aliquet felis vel consequat hendrerit. Vivamus vitae mollis lacus, et consectetur elit. Mauris mattis est quis iaculis ultricies. Integer vestibulum augue ac odio condimentum, quis posuere neque posuere. Nunc feugiat, libero et placerat efficitur, enim dui hendrerit odio, non iaculis sem purus eu urna. Phasellus tincidunt bibendum diam, quis maximus risus.</p>
+					<a href="#IDunnoWhatThisLinksTo"><img class="twitch logo" src="img/twitchLogo.png" alt="Twitch logo"></a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- display navbar -->
+	<div id="navbar">
+
+		<nav class="navbar navbar-default navbar-static-top">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		      	<li><span class="divider"></span></li>
+		        <li><a href="/beta/">Homepage</a></li>
+		        <li><span class="divider"></span></li>
+		        <li><a href="http://www.twitch.tv/team/familia">Members</a></li>
+		        <li><span class="divider"></span></li>
+		        <li><a href="http://twitchfamilia.tv/apply.html">Join the family</a></li>
+		        <li><span class="divider"></span></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+
+	</div>
+</div>
